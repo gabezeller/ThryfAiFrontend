863bbb9d173beb007132a558025bed4c03a3cb06
Gabe Zeller <gabeaz03@gmail.com>
2026-02-07 03:53:44 -0600

    Outfit completer inputs created on frontend

--- src/app/Components/header.tsx
+++ src/app/Components/header.tsx
@@ -12,7 +12,7 @@ import Link from "next/link";
 const Header = () => {
     return (
         // <nav className="flex items-center justify-between w-full bg-gradient-to-tr from-lime-600 via-lime-500 to-amber-400 text-black p-4">
-    <nav className="flex items-center max-w-7xl mx-auto justify-between w-full text-black p-4 sm:rounded-2xl bg-gradient-to-tr from-amber-300  to-amber-400 mb-10 sm:mt-4 rounded-none mt-0">
+    <nav className="flex pl-8 pr-8 items-center max-w-7xl mx-auto justify-between w-full text-black p-4 sm:rounded-2xl bg-gradient-to-tr from-amber-300  to-amber-400 mb-10 sm:mt-4 rounded-none mt-0">
     
             <Link href={"/"} className="text-2xl font-bold  hover:cursor-pointer">
                 <h1 className="text-2xl font-bold  hover:cursor-pointer">Thryft.ai</h1>
