08b1be1d90319935162aa367920c9797dceaa6b2
Gabe Zeller <gabeaz03@gmail.com>
2026-02-07 15:27:08 -0600

    Completed explore page main functionality

--- src/app/Components/item.tsx
+++ src/app/Components/item.tsx
@@ -1,22 +1,35 @@
+import { useState } from "react";
+
 interface ItemCardProps {
   imgUrl: string;
   name: string;
   price: number;
+  heartable?: boolean;
 }
 
-export default function ItemCard({ imgUrl, name, price }: ItemCardProps) {
+export default function ItemCard({ imgUrl, name, price, heartable = true }: ItemCardProps) {
+    const [liked, setLiked] = useState(false);
+
+    const toggleLike = () => {
+        setLiked(!liked);
+    };
+    
     return(
-        <div className="h-64 w-48 rounded-2xl cursor-pointer shadow-xsm hover:shadow-lg transition-shadow duration-300">
+        <div className="h-64 w-48 rounded-2xl cursor-pointer shadow-xsm hover:shadow-lg transition-shadow duration-300 hover:scale-105 transition-transform mx-auto ">
+                {heartable && <svg id="heart" xmlns="http://www.w3.org" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className={` ${liked ? "text-red-500 fill-red-500" : "text-black-500"} w-6 h-6 relative -right-60 top-5 hover:cursor-pointer hover:text-red-500 transition-colors duration-300 ease-in-out transform`} onClick={toggleLike}>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.312-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                </svg>}
             <img className="h-full w-full object-cover rounded-2xl"
                 src={imgUrl}
                 alt={name}
             />
             <h1 className="text-center mt-2">
-            <div className="grid grid-cols-2 gap-4 relative whitespace-nowrap decoration-black">
-                <p>{name} - ${price}</p>
-                <svg id="heart" xmlns="http://www.w3.org" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" className="w-6 h-6 absolute right-0 bottom-0 text-gray-500">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.312-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                </svg>
+            <div className="flex flex-row justify-between relative whitespace-nowrap decoration-black">
+                {/* <p>${price} {name} </p> */}
+                
+                <span>{name}</span>
+                <span >${price}</span>
+  
             </div>
             </h1>
         </div> 
--- src/app/Explore/page.tsx
+++ src/app/Explore/page.tsx
@@ -2,6 +2,19 @@
 import { useEffect } from "react";
 import ItemCard from "../Components/item";
 import { log } from "console";
+import { useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import { Spinner } from "@/components/ui/spinner"
+import { motion } from "framer-motion";
+import Masonry from '@mui/lab/Masonry';
+
 
 interface ExploreItem {
     id: number;
@@ -25,21 +38,164 @@ interface ExplorePageProps {
     price: number[];
 }
 export default function ExplorePage(props: ExplorePageProps) {
+    
+
+    const [exploreItems, setExploreItems] = useState<Product[]>([]);
+    const [featureItems, setFeatureItems] = useState<Product[]>([]);
+
+    const [loading, setLoading] = useState<boolean>(false);
+    
+      // ADD GENDER DROP DOWN
+    async function fetchExploreItems() {
+        setLoading(true);
+   
+
+
+        const url = "http://localhost:5207/api/Discovery/random";
+        console.log("Endpoint URL:", url);
+        try {
+        const response = await fetch(url, {
+            method: "GET"
+        });
+
+
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+
+        }
+
+        const json: Product[] = await response.json();
+        console.log("Explore items:", json);
+        setExploreItems(json);
+
+
+        } catch (error) {
+        console.error("Error fetching items:", error);
+        setExploreItems([]);
+
+        } finally {
+        setLoading(false);
+        }
+
+        return [];
+  }
+
+  async function fetchFeaturedItems() {
+        setLoading(true);
+   
+
+
+        const url = "http://localhost:5207/api/Discovery/personalized?userId=global-user";
+        console.log("Endpoint URL:", url);
+        try {
+        const response = await fetch(url, {
+            method: "GET"
+        });
+
+
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+
+        }
+
+        const json: Product[] = await response.json();
+        console.log("Featured items:", json);
+        setFeatureItems(json);
+
+
+        } catch (error) {
+        console.error("Error fetching items:", error);
+        setFeatureItems([]);
+
+        } finally {
+        setLoading(false);
+        }
+
+        return [];
+  }
+
     useEffect(() => {
-        fetchExploreItems()
-            .then(items => {
-                console.log("Fetched items:", items);
-            })
-            .catch(error => {
-                console.error("Error fetching items:", error);
-            }
-        );
+        fetchExploreItems();
+        fetchFeaturedItems();
     }, []);
+
+    
+      interface Product {
+        id: number;
+        externalId: string;
+        productName: string;
+        gender: string;
+        masterCategory: string;
+        category: string;
+        fashionCategory: string;
+        color: string;
+        season: string;
+        year: number;
+        usage: string;
+        description: string;
+        imageUrl: string;
+        metadata: string;
+        brand: string;
+        price: number;
+      }
+    // useEffect(() => {
+    //     fetchExploreItems()
+    //         .then(items => {
+    //             console.log("Fetched items:", items);
+    //         })
+    //         .catch(error => {
+    //             console.error("Error fetching items:", error);
+    //         }
+    //     );
+    // }, []);
     return(
-        <div className="flex flex-wrap gap-4">
-            {props.imgUrls?.map((url:any, index:any) => (
+        <div className="flex flex-wrap gap-4 w-full ">
+            <motion.div initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }} className="w-full">
+                <h2 className="flex w-9/10 text-5xl font-bold leading-10 tracking-tight mx-auto text-black dark:text-zinc-50 text-left  mb-8 ">Picked for you</h2>
+                {!loading && featureItems.length > 0 ? (<Carousel className="w-9/10  mx-auto"   opts={{
+    align: "start",
+    loop: true,
+  }} plugins={[
+        Autoplay({
+          delay: 2000,
+        stopOnMouseEnter: true,
+        stopOnInteraction: false
+        }),
+      ]}>           <CarouselPrevious />
+                    <CarouselContent>
+                        {featureItems.map((item) => (
+                            <CarouselItem key={item.id} className="xl:basis-1/5 lg:basis-1/3 basis-1/2  p-2 ">
+                                <ItemCard imgUrl={item.imageUrl} name={item.productName} price={item.price} heartable={false} />
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    
+                    <CarouselNext />
+                </Carousel> ) : (
+                    <Spinner className="size-8 mx-auto" />)}
+            {/* </div> */}
+            {/* {props.imgUrls?.map((url:any, index:any) => (
                 <ItemCard key={index} imgUrl={url} name={props.name[index]} price={props.price[index]} />
+            ))} */}
+
+            <motion.div className="w-9/10 mt-10 mx-auto" initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }} >
+                <h2 className=" w-9/10 text-5xl font-bold leading-10 tracking-tight mx-auto text-black dark:text-zinc-50 text-right mb-8 mt-12">Explore more options</h2>
+            {!loading && exploreItems.length > 0 ? (<Masonry columns={4} spacing={8}>
+            {exploreItems.map((item, index) => (
+                <ItemCard key={index} imgUrl={item.imageUrl} name={item.productName} price={item.price} />
+                
             ))}
+            </Masonry>) : (
+                <Spinner className="size-8 mx-auto" />
+            )}
+            </motion.div>
+            </motion.div>
         </div>
     );
 }
