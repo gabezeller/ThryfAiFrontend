87974e6fa7937cfc41f549b52ee1b18751ee20d7
rmeji3 <rafaelm120403@gmail.com>
2026-02-07 10:51:31 -0600

    added masonry layout for landing page

--- src/app/OutfitBuilder/page.tsx
+++ src/app/OutfitBuilder/page.tsx
@@ -419,6 +419,15 @@ function ClothingImageCategory({ img, updateMap }: { img: File, updateMap: (key:
   );
 }
 
+interface Product {
+  productName: string;
+  price: number;
+  category: string;
+  color: string;
+  imageUrl: string;
+  metadata: string;
+}
+
 function ArticleTile({ product }: { product: Product }) {
   return (
     <motion.div
